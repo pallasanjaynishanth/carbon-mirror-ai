04497dd42d810da9e2c31108762bda2245e983e2
pallasanjaynishanth <sanjaynishanth99@gmail.com>
2026-06-16 11:55:30 +0530

    new model 2.5

--- ai/gemini.ts
+++ ai/gemini.ts
@@ -11,7 +11,7 @@ const safetySettings = [
 
 function getModel(vision = false) {
   return genAI.getGenerativeModel({
-    model: 'gemini-2.0-flash',
+    model: 'gemini-2.5-flash',
     safetySettings,
     generationConfig: {
       temperature: 0.8,
