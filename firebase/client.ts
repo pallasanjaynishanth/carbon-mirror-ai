018677a60db104ea91e125afb12a0e6701f782fe
pallasanjaynishanth <sanjaynishanth99@gmail.com>
2026-06-16 13:16:01 +0530

    Debug Firebase env vars

--- firebase/client.ts
+++ firebase/client.ts
@@ -12,9 +12,14 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
+console.log('API KEY:', process.env.NEXT_PUBLIC_FIREBASE_API_KEY)
+console.log('PROJECT ID:', process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID)
+console.log('AUTH DOMAIN:', process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN)
+
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
 export const storage = getStorage(app)
+
 export default app
